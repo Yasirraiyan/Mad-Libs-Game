4b05ce8629e0823785a8e2c4e830509b053b2a52
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-21 15:06:00 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+username = input("What is your name: ")
+fathername = input("What is your father's name: ")
+mothername = input("What is your mother's name: ")
+study = input("Which class you study: ")
+relationship = input("What is your relationship status: ")
+
+print(f"My Name is: {username}, My Father's Name is: {fathername}, My Mother's Name is: {mothername}, I'm studying in: {study}, My relationship status is: {relationship}")
